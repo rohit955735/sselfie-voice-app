112e80c81c485a4f09885dcd3979b046ed25532c
rohit955735 <97736219+rohit955735@users.noreply.github.com>
2023-04-24 16:53:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+var textbox = document.getElementById("textbox");
+function start(){
+    document.getElementById("textbox").innerHTML="";
+    recognition.start();
+}
+recognition.onresult = function(event){
+    console.log(event);
+    var Content = event.results[0][0].transcript;
+    document.getElementById("textbox").innerHTML=Content;
+    console.log(Content);
+speak();
+}
+function speak(){
+ var synth = window.speechSynthesis;
+ var speak_data = document.getElementById("textbox").value;
+ var utterThis = new SpeechSynthesisUtterance(speak_data);
+ synth.speak(utterThis);
+Webcam.attach(camera);
+}
+camera=document.getElementById("camera");
+Webcam.set({
+    width:360,
+    height:250,
+    image_format:'jpeg',
+    jpeg_quality:90
+});
