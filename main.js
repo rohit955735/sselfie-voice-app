251c0a0eedc4777fbb066c2f2792258c9ecfdd01
rohit955735 <97736219+rohit955735@users.noreply.github.com>
2023-04-26 16:45:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,19 +10,42 @@ recognition.onresult = function(event){
     var Content = event.results[0][0].transcript;
     document.getElementById("textbox").innerHTML=Content;
     console.log(Content);
+    if(Content=="take my selfie"){
+        console.log("taking selfie ---");
+    };
+
 speak();
 }
 function speak(){
  var synth = window.speechSynthesis;
- var speak_data = document.getElementById("textbox").value;
+ var speak_data = "taking your selfie in 5 seconds";
  var utterThis = new SpeechSynthesisUtterance(speak_data);
  synth.speak(utterThis);
 Webcam.attach(camera);
-}
+ setTimeout(
+        function(){ 
+            take_snapshot();
+   save();
+     }, 5000);
+  
+  }
+
 camera=document.getElementById("camera");
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="selfie_image" src="'+data_uri+'"/>';
+
+    })
+}
 Webcam.set({
     width:360,
     height:250,
     image_format:'jpeg',
     jpeg_quality:90
 });
+function save(){
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src ;
+    link.href = image;
+    link.click();
+}
